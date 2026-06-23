6fb0d44a5904345ec610c0a8162deacd3fb52952
Ubuntu <ubuntu@ip-172-31-14-11.ap-south-2.compute.internal>
2026-06-23 13:45:59 +0000

    Dockerized Java app deployed on AWS EC2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,5 @@
 public class Main {
-
     public static void main(String[] args) {
-
-        System.out.println("================================");
-        System.out.println(" Dockerized Java Application");
-        System.out.println("================================");
-
-        System.out.println("Developer : Suriya M");
-        System.out.println("Technology : Java + Docker");
-
-        System.out.println("Application started successfully");
-
+        System.out.println("App running on EC2");
     }
-
 }
